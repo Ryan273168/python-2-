d7d0e2fdb262a191439d369424aee5d371d5c2af
ryan273168 <ryan273168@gmail.com>
2023-06-03 15:38:09 +0800

    class12

---
+++ adv-12/prj01.py
@@ -0,0 +1,21 @@
+#######################匯入模組########################
+from pytube import YouTube  # pip install -U pytube
+import os
+import sys
+from function import function as fun
+
+#######################初始化########################
+os.chdir(sys.path[0])  # 將工作目錄設置為目前檔案所在的目錄
+
+#######################主程式########################
+
+u = "https://www.youtube.com/watch?v=WlfIYn8-1pI"
+_, _, _, _, res = fun.get_video_info(u)
+print(res)
+
+r = input("根據上面的資訊, 請輸入要下載的影片的解析度(720p/480p/360p/240p/144p):")
+
+if fun.dwonload_video(u, r):
+    print("下載完成")
+else:
+    print("找不到該解析度的影片")
---
+++ adv-12/prj02.py
@@ -0,0 +1,61 @@
+from ttkbootstrap import *
+import sys
+import os
+from function import function as fun
+
+os.chdir(sys.path[0])
+
+
+def get_video_info_gui():
+    _, _, _, _, res = fun.get_video_info(entry.get())
+
+    res_option['menu'].delete(0, 'end')
+    for r in res:
+        res_option['menu'].add_command(label=r, command=tk._setit(res_var, r))
+    res_var.set(res[0])
+
+
+def download_video_gui():
+    if fun.dwonload_video(entry.get(), res_var.get()):
+        label3.config(text="下載完成")
+    else:
+        label3.config(text="解析錯誤")
+
+
+front_size = 20
+window = tk.Tk()
+window.title("My GUI")
+window.option_add("*font", ("Helvetica", front_size))
+
+style = Style(theme="flatly")
+style.configure('my.TButton', font=("Helvetica", front_size))
+
+label1 = Label(window, text="請輸入YouTube影片網址:")
+label1.grid(row=0, column=0)
+
+lable2 = Label(window, text="請選擇影片解析度")
+lable2.grid(row=1, column=0)
+
+label3 = Label(window, text="下載完成")
+label3.grid(row=2, column=0)
+
+entry = Entry(window, width=20)
+entry.grid(row=0, column=1, padx=10, pady=10)
+
+button = Button(window,
+                text="搜尋影片資訊",
+                command=get_video_info_gui,
+                style="my.TButton")
+button.grid(row=0, column=2)
+
+button = Button(window,
+                text="下載影片",
+                command=download_video_gui,
+                style="my.TButton")
+button.grid(row=1, column=2)
+
+res_var = tk.StringVar()
+res_option = OptionMenu(window, res_var, ())
+res_option.grid(row=1, column=1, padx=10, pady=10)
+
+window.mainloop()
